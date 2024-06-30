77a2c9ddf27cca19623b2018a827cf90c3bd8e9d
SagarNRao <sagarnrao154@gmail.com>
2024-06-30 20:42:51 +0530

    added get posts button - doesnt work yet - almost there

--- blogchain-june24/blogchainapp/src/app/feedre.tsx
+++ blogchain-june24/blogchainapp/src/app/feedre.tsx
@@ -1,31 +1,67 @@
 "use client";
 
-import { useReadContract } from "thirdweb/react";
-import { useQuery } from "@tanstack/react-query";
+import { useReadContract, TransactionButton } from "thirdweb/react";
 import { ValidityContract } from "../../utils/constants";
+import { prepareContractCall } from "thirdweb";
 
-export default function feedre(){
-    const { data, isLoading } = useReadContract({ 
-        ValidityContract, 
-        method: "function getAllposts() view returns ((uint256 id, address username, string postText, bool isDeleted)[])", 
-        params: [] 
-      });
-    }
+export const Feedre: React.FC = () => {
+  const {
+    data: posts,
+    isLoading: loadingposts,
+    refetch,
+  } = useReadContract({
+    contract: ValidityContract,
+    method: "getAllposts",
+  });
 
+  console.log("here");
+  console.log(posts);
   return (
     <div>
+      <TransactionButton
+        transaction={() =>
+          prepareContractCall({
+            contract: ValidityContract,
+            method: "getAllposts",
+          })
+        }
+        onError={(error) => console.log("oof", error)}
+        onTransactionConfirmed={() => refetch}
+      >
+        {"getposts"}
+      </TransactionButton>
       {loadingposts ? (
         <h2>...</h2>
       ) : (
         <div>
           <h2>Posts</h2>
-          {posts?.map((post, index) => (
-            <div key={index}>
-              <p>{post.postText}</p>
-            </div>
-          ))}
+          {posts &&
+            posts.map((post: any, index: number) => (
+              <div key={index}>
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+              </div>
+            ))}
         </div>
       )}
     </div>
   );
 };
+
+// the function that fetches all the posts (getAllposts in contract)
+// function getAllposts() external view returns (post[] memory) {
+//     post[] memory temporary = new post[](posts.length);
+//     uint counter = 0;
+//     for(uint i=0; i<posts.length; i++) {
+//         if(posts[i].isDeleted == false) {
+//             temporary[counter] = posts[i];
+//             counter++;
+//         }
+//     }
+
+//     post[] memory result = new post[](counter);
+//     for(uint i=0; i<counter; i++) {
+//         result[i] = temporary[i];
+//     }
+//     return result;
+// }
